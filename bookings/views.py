3d841f09698b7ba00b79bf2fce40e4bca0e04c32
bjarreau <brittney.jarreau@nrlssc.navy.mil>
2026-05-04 13:40:27 -0500

    add the ability to get the list of ships

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Ship
 
 # Create your views here.
+def get_ships(request):
+    ships = list(Ship.objects.values('id', 'name'))
+    return JsonResponse(ships, safe=False)
